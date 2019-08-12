79fb4e2f9d9774f862dac6c3959a99f0e872a7e5
kmn5409 <nichols.keanu9@gmail.com>
2019-08-12 19:13:09 -0400

    We now have both the used and new prices of the books

--- reading.py
+++ reading.py
@@ -2,10 +2,9 @@ import pandas as pd
 
 def count_books_and_ratings(books, ratings):
     ISBN_s = [row['ISBN'] for index,row in books.iterrows()]
-    print('ISBN_s:')
-    print(type(ISBN_s[0]))
     i = 0
-    print(books['prices'][i])
+    print('Prints the books and the frequency and the used prices of the books: \n')
+    print('ISBN\t\t Count\t Price')
     for isbn in ISBN_s:
         counts = ratings['ISBN'].value_counts().to_dict()
         print(isbn, '\t' , counts[isbn], '\t', books['prices'][i])
@@ -13,18 +12,19 @@ def count_books_and_ratings(books, ratings):
     #counts = [isbn,count for isbn,count in ISBN_s,ratings.groupby(isbn).count()] 
 
 def read_ratings():
+    #print('BX-Book-Ratings file: ')
     ratings = pd.read_csv('BX-Book-Ratings.csv', sep=';',encoding='unicode_escape')
-    print(ratings.head())
-    print(ratings.tail())
+    #print(ratings.head())
     return ratings
 
 def read_books():
     books = pd.read_csv('BX-Books.csv', sep=';', nrows=20)
-    print(books.head())
-    prices = [64.68,2.50,65.73,7.97,8.60,8.90,9.56,4.99,14.95,12.00,9.15,16.00,79.30,4.95,2.00,7.95,75.55,10.91,2.19,21.00]
-    books['prices'] = prices
-    print('\nFirst book: ')
-    print(books.iloc[0])
+    #print(books.head())
+    new_prices = [64.68,2.50,65.73,7.97,8.60,8.90,9.56,4.99,14.95,12.00,9.15,16.00,79.30,4.95,2.00,7.95,75.55,10.91,2.19,21.00]
+    used_prices = [2.30,1.80,1.99,0.71,0.25,1.09,0.10,0.49,1.96,2.00,0.69,8.86,15.43,4.95,1.14,9.99,6.51,1.50,0.01,1.00]
+    books['prices'] = used_prices
+    #print('\nFirst book: ')
+    #print(books.iloc[0])
     return books
 
 def main():
@@ -35,3 +35,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
